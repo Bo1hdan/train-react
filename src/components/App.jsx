2a0eebc4466e72a755e21d09c3c2e107cf7786d3
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2023-12-01 17:04:35 -0700

    963

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import { EventBoard } from './EventBoard/EventBoard';
 import upcomingEvents from '../upcoming-events.json';
 export const App = () => {
   return (
-    <div>
+    <>
       <PageTitle text="24th Core Worlds Coalition Conference" />
       <EventBoard events={upcomingEvents} />
-    </div>
+    </>
   );
 };
--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,29 +1,42 @@
 import PropTypes from 'prop-types';
+import css from './Event.module.css';
+
+import { formatEventStart, formatEventDuration } from 'utils';
+
+import {
+  FaMapMarkerAlt,
+  FaCalendarAlt,
+  FaUserAlt,
+  FaClock,
+} from 'react-icons/fa';
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
+  const formattedStart = formatEventStart(start);
+  const duration = formatEventDuration(start, end);
   return (
-    <div class="event">
-      <h2 class="title">Name</h2>
-      <p class="info">
-        <i class="icon"></i>
-        Location
+    <div className={css.event}>
+      <h2 className={css.title}>{name}</h2>
+      <p className={css.info}>
+        <FaMapMarkerAlt className={css.icon} />
+        {location}
       </p>
-      <p class="info">
-        <i class="icon"></i>
-        Speaker
+      <p className={css.info}>
+        <FaUserAlt className={css.icon} />
+        {speaker}
       </p>
-      <p class="info">
-        <i class="icon"></i>
-        Start Date
+      <p className={css.info}>
+        <FaCalendarAlt className={css.icon} />
+        {formattedStart}
       </p>
-      <p class="info">
-        <i class="icon"></i>
-        Duration
+      <p className={css.info}>
+        <FaClock className={css.icon} />
+        {duration}
       </p>
-      <span class="chip free|paid|vip">Event type</span>
+      <span className={` ${css.chip}  ${css[type]}`}>{type} </span>
     </div>
   );
 };
+// chip free|paid|vip
 
 Event.propTypes = {
   name: PropTypes.string.isRequired,
